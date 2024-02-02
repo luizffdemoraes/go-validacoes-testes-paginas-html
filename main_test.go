c69166e95cdfbea7d284c600961f538ce4794c47
Luiz Felipe <lffm1994@gmail.com>
2024-02-02 19:36:22 -0300

    StatusCode

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"api-go-gin/controllers"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -13,4 +16,15 @@ func SetupDasRotasDeTeste() *gin.Engine {
 
 func TestFalha(t *testing.T) {
 	t.Fatalf("Teste falhou de propósito, não se preocupe")
+}
+
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/gui", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	if resposta.Code != http.StatusOK {
+		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
+	}
 }
